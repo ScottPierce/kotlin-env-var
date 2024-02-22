88e98f2992139f37aafd0770b35759f4081b80b8
Scott Pierce <871169+ScottPierce@users.noreply.github.com>
2024-02-21 22:30:06 -0500

    lets try this

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     kotlin("multiplatform") version "1.9.22"
+    id("com.vanniktech.maven.publish") version "0.27.0"
 }
 
 group = "dev.scottpierce"
@@ -11,6 +14,7 @@ repositories {
 
 kotlin {
     jvm()
+
     js {
         browser()
         nodejs()
@@ -19,18 +23,22 @@ kotlin {
     iosArm64()
     iosX64()
     iosSimulatorArm64()
+
     tvosArm64()
     tvosSimulatorArm64()
     tvosX64()
+
     macosArm64()
     macosX64()
+
+    mingwX64()
 
     applyDefaultHierarchyTemplate()
 
     sourceSets {
         getByName("jsMain") {
             dependencies {
-                implementation("org.jetbrains.kotlin-wrappers:kotlin-node:18.16.12-pre.687")
+                implementation("org.jetbrains.kotlin-wrappers:kotlin-node:18.11.19-pre.494")
             }
         }
     }
@@ -46,5 +54,40 @@ tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompileCommon> {
     kotlinOptions {
         freeCompilerArgs += listOf("-Xexpect-actual-classes")
+    }
+}
+
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.DEFAULT)
+//    publishToMavenCentral(SonatypeHost.S01)
+
+    signAllPublications()
+
+    coordinates("dev.scottpierce", "kotlin-env-var", "1.0.0")
+
+    pom {
+        name.set("Kotlin Environment Variables")
+        description.set("A Kotlin Multiplatform library for retrieving environment variables")
+        inceptionYear.set("2024")
+        url.set("https://github.com/ScottPierce/kotlin-env-var")
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+        developers {
+            developer {
+                id.set("ScottPierce")
+                name.set("Scott Pierce")
+                url.set("https://github.com/ScottPierce")
+            }
+        }
+        scm {
+            url.set("https://github.com/ScottPierce/kotlin-env-var")
+            connection.set("scm:git:git://github.com/ScottPierce/kotlin-env-var.git")
+            developerConnection.set("scm:git:ssh://git@github.com/ScottPierce/kotlin-env-var.git")
+        }
     }
 }
--- src/jsMain/kotlin/dev/scottpierce/envvar/EnvVarPlatform.js.kt
+++ src/jsMain/kotlin/dev/scottpierce/envvar/EnvVarPlatform.js.kt
@@ -1,6 +1,7 @@
 package dev.scottpierce.envvar
 
 import node.process.process
+import js.core.get
 
 internal actual object EnvVarPlatform {
     actual operator fun get(name: String): String? {
